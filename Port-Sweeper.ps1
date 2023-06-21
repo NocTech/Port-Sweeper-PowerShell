5e93214c85ebd09f0314e46d2dfcc7dbb9407f86
Jonas Nygren <jonas_nygren30@hotmail.com>
2023-06-21 17:15:23 +0200

    Port-Sweeper Upload

---
+++ Port-Sweeper.ps1
@@ -0,0 +1,16 @@
+﻿$computer = "localhost"  # Replace with the target computer name or IP address
+$startPort = 1  # Specify the starting port number
+$endPort = 65535  # Specify the ending port number
+
+$colorOffline = "Red"
+$colorOnline = "Green"
+
+for ($port = $startPort; $port -le $endPort; $port++) {
+    $result = Test-NetConnection -ComputerName $computer -Port $port -InformationLevel "Quiet" -WarningAction SilentlyContinue
+
+    if ($result.TcpTestSucceeded) {
+        Write-Host "Port $port is open" -ForegroundColor $colorOnline
+    } else {
+        Write-Host "Port $port is closed" -ForegroundColor $colorOffline
+    }
+}
